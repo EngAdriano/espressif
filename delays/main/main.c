25d88a5ab10827b0d9b85bce8e249815c6bee0c8
José Adriano <adriano.liukin@gmail.com>
2026-03-08 10:49:34 -0300

    Atualização

---
+++ delays/main/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO GPIO_NUM_48
+
+void app_main(void)
+{
+    esp_rom_gpio_pad_select_gpio(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        gpio_set_level(BLINK_GPIO, 0);
+        printf("Ligado\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_GPIO, 1);
+        printf("Desligado\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
